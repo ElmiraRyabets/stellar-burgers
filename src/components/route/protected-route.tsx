a30c80dde896256c439ae1617b7372809b20ab91
Elmira_Spitchenko <Elmira-Ryabets@yandex.ru>
2025-12-13 23:17:18 +0700

    fix review comments

--- src/components/route/protected-route.tsx
+++ src/components/route/protected-route.tsx
@@ -24,7 +24,7 @@ export const ProtectedRoute = ({
     return <Navigate replace to='/login' state={{ from: location }} />;
   }
 
-  if (onlyUnAuth && isAuth) {
+  if (onlyUnAuth && isAuth && status == 'succeeded') {
     const from = location.state?.from || { pathname: '/' };
 
     return <Navigate replace to={from} />;
